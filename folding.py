5df34d7e687d06058838771ada2bcf1eb8734a5a
Colin Bate <colin@colinbate.com>
2014-09-24 23:45:42 -0300

    Update and it is now working

--- folding.py
+++ folding.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import subprocess
+import sublime
 import sublime_plugin
 
-class SendToFoldingTextCommand(sublime_plugin.WindowCommand):
+class FoldingCommand(sublime_plugin.WindowCommand):
     def run(self):
         filename = self.window.active_view().file_name()
         if filename is None:
@@ -14,15 +15,7 @@ class SendToFoldingTextCommand(sublime_plugin.WindowCommand):
         for k, v in proc_env.items():
             proc_env[k] = os.path.expandvars(v).encode(encoding)
 
-        
-        try:
-            subprocess.check_call(
-                ['open', '-a', 'FoldingText', filename],
-                env=proc_env
-            )
-            break
-        except subprocess.CalledProcessError:
-            pass
+        subprocess.call(['open', '-a', 'FoldingText', filename], env=proc_env)
 
     def is_enabled(self):
         return True
